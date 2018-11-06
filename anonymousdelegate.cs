d8e5ea1cd04a6ea167f9cccf06706044c2b88946
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-11-06 21:03:04 +0530

    Add files via upload

---
+++ anonymousdelegate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace anonymous_delegates
+{
+    public delegate string GreetingsDelegate(string name);
+    class Program
+    {
+
+        //public static string Greetings(string Name)
+        //{
+        //    return "hello    " + Name + "   A very Good Morning";
+        //}
+        static void Main()
+        {
+           //GreetingsDelegate obj = new GreetingsDelegate(Greetings);
+            GreetingsDelegate obj = delegate(string name)//anonymous delegate
+            {
+                return "hello    " + name + "   A very Good Morning";
+            };
+            string str = obj.Invoke("scott");
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ lamda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lambda_expressions
+{
+   public delegate string GreetingsDelegate(string Name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetingsDelegate obj =( Name)=>
+            {
+                return "hello    " + Name +   "   a very Good Morning ";
+            };
+
+            string str = obj.Invoke("sajan");
+            Console.WriteLine(str);
+            Console.ReadLine();
+        
+
+        }
+    }
+}
